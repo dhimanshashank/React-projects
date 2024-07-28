3772f7de32451744063b12ae47fbf07e5f774c5f
dhimanshashank <dhimanshashank15@gmail.com>
2024-07-28 18:35:39 +0530

    Modify changes

--- 2-bharat-clock-version-one/src/components/CurrentTime.jsx
+++ 2-bharat-clock-version-one/src/components/CurrentTime.jsx
@@ -2,7 +2,7 @@ let CurrentTime = () => {
 
     let time = new Date();
 
-    return <p>This is the current time in Bharat : {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>;
+    return <p id= "current-time"> {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>;
 };
 
 export default CurrentTime;
