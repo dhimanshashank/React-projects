7c5d626582ae277e13c18d4b0a187c7ec8691d87
dhimanshashank <dhimanshashank15@gmail.com>
2024-08-05 23:48:57 +0530

    Adding calculator project - version 1

--- 4-calculator-version-one/calculator/src/App.jsx
+++ 4-calculator-version-one/calculator/src/App.jsx
@@ -1,13 +1,22 @@
 import styles from "./App.module.css";
+import Screen from "./components/Screen";
 
 function App() {
   return (
-    <div className={styles.calculator}>
-      <input type="text" className={styles.display} />
-      <div className={styles.buttons - container}>
-        <button>C</button>
+    <>
+      <div className={styles.calculator}>
+        <Screen />
+
+        <div className={styles.buttons}>
+          <div className={styles.row}>
+            <button className={styles.button}>AC</button>
+            <button className={styles.button}>+/-</button>
+            <button className={styles.button}>%</button>
+            <button className={styles.button}>/</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ 4-calculator-version-one/calculator/src/components/Screen.jsx
@@ -0,0 +1,12 @@
+import styles from "./Screen.module.css";
+
+function Screen() {
+    return (
+      <>
+        <input type="text" placeholder="Enter the number" />
+      </>
+    );
+  }
+  
+export default Screen;
+  
